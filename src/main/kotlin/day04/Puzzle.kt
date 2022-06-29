a3eed323596f7ead48e4eb3e78cb5a56eb3fc0fc
cmgdev <854806+cmgdev@users.noreply.github.com>
2022-06-29 11:41:40 -0500

    day 4, part 1

---
+++ src/main/kotlin/day04/Puzzle.kt
@@ -0,0 +1,25 @@
+package day04
+
+import java.security.MessageDigest
+
+val md5: MessageDigest = MessageDigest.getInstance("MD5")
+
+fun main() {
+    println(findHashStartingWith(EXAMPLE_1, "00000") == 609043)
+    println(findHashStartingWith(EXAMPLE_2, "00000") == 1048970)
+    println(findHashStartingWith(INPUT, "00000") == 254575)
+}
+
+fun findHashStartingWith(secretKey: String, target: String): Int? {
+    return (1..Int.MAX_VALUE).find { i ->
+        md5.digest("$secretKey$i".toByteArray())
+            .take(3)
+            .joinToString("") { "%02x".format(it) }
+            //.also { println("$i -> $it") }
+            .startsWith(target)
+    }
+}
+
+const val EXAMPLE_1 = "abcdef"
+const val EXAMPLE_2 = "pqrstuv"
+const val INPUT = "bgvyzdsv"
