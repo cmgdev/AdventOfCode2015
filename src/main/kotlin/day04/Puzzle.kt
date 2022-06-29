3b9fcc856d01e7726d8ffe16d0585f9a5356106c
cmgdev <854806+cmgdev@users.noreply.github.com>
2022-06-29 11:44:50 -0500

    day 4, part 2

--- src/main/kotlin/day04/Puzzle.kt
+++ src/main/kotlin/day04/Puzzle.kt
@@ -8,6 +8,8 @@ fun main() {
     println(findHashStartingWith(EXAMPLE_1, "00000") == 609043)
     println(findHashStartingWith(EXAMPLE_2, "00000") == 1048970)
     println(findHashStartingWith(INPUT, "00000") == 254575)
+
+    println(findHashStartingWith(INPUT, "000000") == 1038736)
 }
 
 fun findHashStartingWith(secretKey: String, target: String): Int? {
@@ -15,7 +17,7 @@ fun findHashStartingWith(secretKey: String, target: String): Int? {
         md5.digest("$secretKey$i".toByteArray())
             .take(3)
             .joinToString("") { "%02x".format(it) }
-            //.also { println("$i -> $it") }
+//            .also { if (i % 100000 == 0) println("$i -> $it") }
             .startsWith(target)
     }
 }
