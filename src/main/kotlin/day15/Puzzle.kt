0cfdbd123c1b67e7cb473fe9bee9575a0b74044c
cmgdev <854806+cmgdev@users.noreply.github.com>
2022-07-09 13:18:54 -0500

    day 15, part 2

--- src/main/kotlin/day15/Puzzle.kt
+++ src/main/kotlin/day15/Puzzle.kt
@@ -6,40 +6,48 @@ fun main() {
     val butterscotch = Ingredient(-1, -2, 6, 3, 8)
     val cinnamon = Ingredient(2, 3, -2, -1, 3)
 
-    var maxScore = 0
-    (0..100).forEach { b ->
-        val c = 100 - b
-        val score = Recipe(listOf(Pair(butterscotch, b), Pair(cinnamon, c))).getScore()
-        maxScore = max(maxScore, score)
-    }
-    println(maxScore == 62842880)
+    println(maxOf2(butterscotch, cinnamon) == 62842880)
+    println(maxOf2(butterscotch, cinnamon, 500) == 57600000)
 
     val sugar = Ingredient(3, 0, 0, -3, 2)
     val sprinkles = Ingredient(-3, 3, 0, 0, 9)
     val candy = Ingredient(-1, 0, 4, 0, 1)
     val chocolate = Ingredient(0, 0, -2, 2, 8)
-    maxScore = 0
-    (0..100).forEach { su ->
-        (0..100 - su).forEach { sp ->
-            (0..100 - su - sp).forEach { ca ->
-                val ch = 100 - su - sp - ca
-                val score = Recipe(
-                    listOf(
-                        Pair(sugar, su),
-                        Pair(sprinkles, sp),
-                        Pair(candy, ca),
-                        Pair(chocolate, ch)
-                    )
-                ).getScore()
+    println(maxOf4(sugar, sprinkles, candy, chocolate) == 222870)
+    println(maxOf4(sugar, sprinkles, candy, chocolate, 500) == 117936)
+}
+
+private fun maxOf4(
+    a: Ingredient, b: Ingredient, c: Ingredient, d: Ingredient, constrainCalories: Int = 0
+): Int {
+    var maxScore = 0
+    (0..100).forEach { i ->
+        (0..100 - i).forEach { j ->
+            (0..100 - i - j).forEach { k ->
+                val l = 100 - i - j - k
+                val score = Recipe(listOf(Pair(a, i), Pair(b, j), Pair(c, k), Pair(d, l))).getScore(constrainCalories)
                 maxScore = max(maxScore, score)
             }
         }
     }
-    println(maxScore == 222870)
+    return maxScore
+}
+
+private fun maxOf2(a: Ingredient, b: Ingredient, constrainCalories: Int = 0): Int {
+    var maxScore = 0
+    (0..100).forEach { i ->
+        val j = 100 - i
+        val score = Recipe(listOf(Pair(a, i), Pair(b, j))).getScore(constrainCalories)
+        maxScore = max(maxScore, score)
+    }
+    return maxScore
 }
 
 data class Recipe(val ingredients: List<Pair<Ingredient, Int>>) {
-    fun getScore(): Int {
+    fun getScore(constrainCalories: Int = 0): Int {
+        if (constrainCalories > 0 && ingredients.sumOf { it.first.calories * it.second } != constrainCalories) {
+            return 0
+        }
         val capacity = ingredients.sumOf { it.first.capacity * it.second }.coerceAtLeast(0)
         val durability = ingredients.sumOf { it.first.durability * it.second }.coerceAtLeast(0)
         val flavor = ingredients.sumOf { it.first.flavor * it.second }.coerceAtLeast(0)
